3b455608c4ca899deaaa9c9852a349337bb3cd64
ShazleenAziem <shazleenaziem10@gmail.com>
2022-10-25 10:15:39 +0800

    login

---
+++ script.js
@@ -0,0 +1,13 @@
+function login()
+{
+    let password = document.getElementById("password").value;
+    if(password.length <= 12)
+    {
+        alert("not enough")
+    }
+        else
+    {
+        alert("password");
+    }
+    
+}
